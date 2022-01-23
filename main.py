9ac70b96ed73b106f358f050b43177763bed50e0
Dev Arora <93930157+Dev-Arora240@users.noreply.github.com>
2022-01-23 06:16:59 +0000

    h

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-"""
-Name(s):
-Name of Project:
-"""
 
-#Write the main part of your program here. Use of the other pages is optional.
+"""Name(s): Dev Arora and Sean Kaloudis
+Name of Project: Adventure Game Final Project"""
 
-#import page1  # uncomment if you're using page1
-#import page2  # uncomment if you're using page2
-#import page3  # uncomment if you're using page3
-#import page4  # uncomment if you're using page4
+def enter():
+    print ("Congrats, you have entered the game! You are faced with your first challenge. You see a ")
+    
+
+def start():
+  print("Welcome to Tower Defense. You are a ruthless commander whose only objective is to defend your own tower and crush your enemy.")
+  choice = input("Enter A or a to enter game: ")
+  if choice in ("A","a"): 
+    enter()
+  else:
+    print ("You are banished. If you can't even type in A you are not worthy of being a commander in the legendary Tower Defense. Please restart the game or accept your defeat. ")
+
